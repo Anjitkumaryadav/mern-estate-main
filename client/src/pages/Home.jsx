d4699ad158a45b8584b3e3406da5ab6d346f4222
Anjitkumaryadav <anjitaniket@gmail.com>
2024-03-27 02:45:39 +0530

    final

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -50,15 +50,15 @@ export default function Home() {
       {/* top */}
       <div className='flex flex-col gap-6 p-28 px-3 max-w-6xl mx-auto'>
         <h1 className='text-slate-700 font-bold text-3xl lg:text-6xl'>
-          Find your next <span className='text-slate-500'>perfect</span>
+        Your Gateway to<span className='text-slate-500'>Possibilities</span>
           <br />
-          place with ease
+          Discover, List, Rent
         </h1>
         <div className='text-gray-400 text-xs sm:text-sm'>
           Real Estate System is the best place to find your next perfect place to
           live.
           <br />
-          We have a wide range of properties for you to choose from.
+          We have a wide range of properties and things for you to choose from.
         </div>
         <Link
           to={'/search'}
